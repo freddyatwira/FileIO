aa0045a4571e9e47dd109b06279e2ad5ec05ba71
freddyatwira <freddywaigwa@gmail.com>
2026-02-12 18:19:23 +0300

    added csv with commas

--- students.py
+++ students.py
@@ -2,6 +2,8 @@ import csv
 
 students = []
 
+#using csv module to read the csv file and create a list of dictionaries
+
 with open("students.csv") as file:
     reader = csv.DictReader(file, fieldnames=["name", "town",])
     for row in reader:
@@ -10,7 +12,7 @@ with open("students.csv") as file:
 
 
 
-# used when csv has single comma as a separator
+# used when csv has single comma as a separator 
 """    for line in f:
         name, town = line.rstrip().split(",")
         student = {"name": name, "town": town}
